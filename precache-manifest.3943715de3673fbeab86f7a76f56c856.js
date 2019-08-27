b0095809fb0cb15b80f64e779ecde3a91778c5a6
czekalam <1marcin1@vp.pl>
2019-08-27 14:52:44 +0200

    Updates

---
+++ precache-manifest.3943715de3673fbeab86f7a76f56c856.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "5e35f5b1a2b46ab4bde3",
+    "url": "/weather/static/js/main.8eff3ec5.chunk.js"
+  },
+  {
+    "revision": "0805098e377dc57d50f3",
+    "url": "/weather/static/js/2.2d4b6854.chunk.js"
+  },
+  {
+    "revision": "5e35f5b1a2b46ab4bde3",
+    "url": "/weather/static/css/main.45960ff8.chunk.css"
+  },
+  {
+    "revision": "8a5514822d60e110a476667460ad250f",
+    "url": "/weather/index.html"
+  }
+];
